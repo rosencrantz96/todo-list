507bbb41301730ade48c56ff339eeceab16e4f2d
rosencrantz96 <topgdvidsyb@naver.com>
2023-02-02 15:20:37 +0900

    달력 만드는 중

---
+++ src/main/resources/static/js/calanderScript.js
@@ -0,0 +1,8 @@
+	<script th:inline="javascript">
+		const date = new Date();
+
+		const viewYear = date.getFullYear();
+		const viewMonth = date.getMonth();
+
+		document.querySelector('.year-monthBox').textContent = '${viewYear}년 ${veiwMonth + 1}월';
+	</script>

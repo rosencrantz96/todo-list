3d38d0084b79f1c850fcbebfdfdb1192fc6d4a37
rosencrantz96 <topgdvidsyb@naver.com>
2023-02-06 01:06:51 +0900

    회원가입, 로그인... 다시 하기...

--- src/main/java/com/todolist/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todolist.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.todolist.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-	Member findByEmail(String email); // 회원가입 시 중복 회원이 있는지 검사하기 위해
-}

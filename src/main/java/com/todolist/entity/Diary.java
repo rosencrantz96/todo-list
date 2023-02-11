3343c0ec055ee98f99a325fdfa950d67a48b2dda
rosencrantz96 <topgdvidsyb@naver.com>
2023-02-11 23:47:11 +0900

    엔티티, dto 구성

--- src/main/java/com/todolist/entity/Diary.java
+++ src/main/java/com/todolist/entity/Diary.java
@@ -25,6 +25,7 @@ import lombok.ToString;
 @ToString
 public class Diary {
 	// TODO : 마찬가지로... 일기도 '하루'에 담겨야 하는거니까... 날짜 객체에 일기를 담아야 하는 것 아닐까? 일정(들)에 일기가 담기는 것이 아니라 날짜 하루에 일기가 담기는 거니까... 엔티티/dto 설계를 다시 해야 할 듯... 
+	// TODO 날짜를... 어떻게 다룰까...
 	
 	@Id
 	@Column(name = "diary_id")

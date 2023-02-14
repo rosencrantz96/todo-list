c259bfa45cf3cb3a8a7bd94f15ffb1ff3df8063a
rosencrantz96 <topgdvidsyb@naver.com>
2023-02-14 18:22:55 +0900

    화면 다시

--- src/main/resources/static/js/todo.js
+++ src/main/resources/static/js/todo.js
@@ -1,40 +1,8 @@
-let date = new Date();
+date = new Date();
 
-const renderCalender = () => {
-  const viewYear = date.getFullYear();
-  const viewMonth = date.getMonth();
-  const viewDate = date.getDate();
-  
-  const weekday = new Array(7);
-  weekday[0] = "일";
-  weekday[1] = "월";
-  weekday[2] = "화";
-  weekday[3] = "수";
-  weekday[4] = "목";
-  weekday[5] = "금";
-  weekday[6] = "토";
-  const viewDay = weekday[date.getDay()];
-  
-	// year-month 채우기
-  document.querySelector('.year-month').textContent = `${viewMonth + 1}월 ${viewDate}일 ${viewDay}요일`;
-};
+year = date.getFullYear();
+month = date.getMonth() + 1;
+day = date.getDate();
 
-renderCalender();
-
-const prevMonth = () => {
-  date.setMonth(date.getMonth() - 1);
-  renderCalender();
-};
-
-const nextMonth = () => {
-  date.setMonth(date.getMonth() + 1);
-  renderCalender();
-};
-
-const goToday = () => {
-  date = new Date();
-  renderCalender();
-};
-
-
+document.getElementById("go-today").innerHTML = year + "년 " + month + "월 " + day + "일";
 
